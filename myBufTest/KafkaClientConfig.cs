b37d4f245b72bd9eb39c43032a321f2c1ce30469
Robert Fu <robert.fu@confluent.io>
2021-08-25 16:21:32 +1000

    Upload Kafka Client config

---
+++ myBufTest/KafkaClientConfig.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Confluent.Kafka;
+using Confluent.SchemaRegistry;
+
+namespace RF.myBufTest
+{
+    public static class KafkaClientConfig
+    {
+        private static string _bootstrapServer = "";
+        private static string _srURL = "";
+        private static string _saslUsername = "";
+        private static string _saslPassword = "";
+
+        public static ProducerConfig pConfig = new ProducerConfig
+        {
+            BootstrapServers = _bootstrapServer,
+            SaslMechanism = SaslMechanism.Plain,
+            SecurityProtocol = SecurityProtocol.SaslSsl,
+            SaslUsername = _saslUsername,
+            SaslPassword = _saslPassword
+        };
+
+
+        public static ConsumerConfig cConfig = new ConsumerConfig
+        {
+            BootstrapServers = _bootstrapServer,
+            SaslMechanism = SaslMechanism.Plain,
+            SecurityProtocol = SecurityProtocol.SaslSsl,
+            SaslUsername = _saslUsername,
+            SaslPassword = _saslPassword,
+            GroupId = Guid.NewGuid().ToString(),
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        public static SchemaRegistryConfig srConfig = new SchemaRegistryConfig
+        {
+            Url = _srURL,
+            BasicAuthUserInfo = ""
+        };
+    }
+}
